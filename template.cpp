3418d72d0d243c00c454fdf14b18a4ed21c58590
Shinee <relaxdhit@gmail.com>
2023-11-25 22:31:40 +0700

    first commit

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define debug(x) cerr << #x << " --> " << x << endl;
+#define shine ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr);
+
+int main() {
+    shine
+
+}
