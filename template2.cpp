f4e5c93959a557f1fd38f6748fe8854d0e56d8c5
Radhit Akriandra <115268091+Shineee01@users.noreply.github.com>
2023-12-27 15:21:09 +0700

    Add files via upload

---
+++ template2.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <vector>
+#include <climits>
+#include <deque>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+#define endl '\n'
+#define debug(x) cerr << #x << " --> " << x << endl;
+#define shine ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+int main(){
+    shine
+    
+}
