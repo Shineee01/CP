752655de649b65f79d3d17ecbfe74b8988d0e45e
Radhit Akriandra <115268091+Shineee01@users.noreply.github.com>
2023-12-27 17:19:25 +0700

    Add files via upload

---
+++ bitset.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n' 
+#define debug(x) cerr << #x << " --> " << x << endl;
+#define shine ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr);
+string biner(int n) {
+    string res = "";
+    while(n){
+        res = char('0' + n % 2) + res;
+        n >>= 1;
+    }
+    return res.empty() ? "0" : res;
+}
+int main(){
+    shine
+    int a, b;
+    cin >> a >> b;
+    cout << a << " & " << b << " = " << (a & b) << " ------> " << biner(a) << " & " << biner(b) << " = " << biner(a & b) << endl << endl;
+    cout << a << " ^ " << b << " = " << (a ^ b) << " ------> " << biner(a) << " ^ " << biner(b) << " = " << biner(a ^ b) << endl << endl;
+    cout << a << " | " << b << " = " << (a | b) << " ------> " << biner(a) << " | " << biner(b) << " = " << biner(a | b) << endl << endl;
+    cout << a << " >> " << b << " = " << (a >> b) << " ------> " << biner(a) << " >> " << biner(b) << " = " << biner(a >> b) << endl << endl;
+    cout << a << " << " << b << " = " << (a << b) << " ------> " << biner(a) << " << " << biner(b) << " = " << biner(a >> b) << endl << endl;
+}
